00800291ebe53a0b7e82da4d469222a95526ef2a
markusguenther <mukguenther@gmail.com>
2019-01-12 23:54:40 +0100

    Fix: Use variables also in imported css files

--- DistributionPackages/NeosBlogSpace.Site/postcss.config.js
+++ DistributionPackages/NeosBlogSpace.Site/postcss.config.js
@@ -3,13 +3,13 @@ const styles = require('./Resources/Private/JavaScript/styleConstants');
 module.exports = {
     plugins: [
         require('autoprefixer'),
+        require('postcss-import')(),
         require('cssnano')({
             discardComments: { removeAll: true },
         }),
         require('postcss-css-variables')({
             variables: Object.assign(styles.styles)
         }),
-        require('postcss-import')(),
         require('postcss-nested')(),
         require('postcss-hexrgba')()
     ]
